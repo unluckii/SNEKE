2ee03db8243fd1773ba2ed43ad2ae5da0873dbe5
trigujus001 <trigujus001@warren.k12.in.us>
2025-11-07 12:23:24 -0500

    day 1 submitted

--- day01/snake.py
+++ day01/snake.py
@@ -6,14 +6,42 @@ pygame.init()
 # Set display window
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+size = (WINDOW_WIDTH, WINDOW_HEIGHT)
+display_surface = pygame.display.set_mode(size)
 pygame.display.set_caption("~~SNEKE~~")
 
 # Set FSP and clock
+# todo: make a variable (constant) called FPS and initialize to 20
+FPS = 20
+clock = pygame.time.Clock()
 
 # Set game values
+# TODO: make a variable (constant) named SNAKE_SIZE and initialize to 20
+SNAKE_SIZE = 20
+
+# TODO: make a variable named head_x and assignment half of the WINDOW_WIDTH to it. use integer division // (i.e. 11/ 2 is 5.5, 11//2 is 5
+head_x = WINDOW_WIDTH / 2
+# TODO: make a variable named head_y and assign half og the WINDOW_HEIGHT + 100 to it. use integer division //
+head_y = WINDOW_HEIGHT / 2 + 100
+
+# TODO: make a variable named snake_dx and assign 0 to it
+snake_dx = 0
+# TODO: repeat for a variable named snake_dy
+snake_dy = 0
+
+#TODO: make a variable named score and assign 0 to it
+score = 0
 
 # Set colors
+GREEN = (0, 255, 0) # (r, g , b)
+# TODO: make a DARKGREEN color with rgb (10,50,10)
+DARKGREEN = (10, 150, 10)
+#TODO make a RED
+RED = (255, 0, 0)
+# TODO: make a DARKGREEN with rgb of (150, 0, 0)
+DARKRED = (150, 0, 0)
+# TODO: make a WHITE
+WHITE = (255, 255, 255)
 
 # Set fonts
 
@@ -35,7 +63,7 @@ while running:
         # Move the snake
 
     # Add the head coordinate to the first index of the body coordinate list
-    # This will essentilalyl move all of the snakes body by one position in the list
+    # This will essentially move all the snakes body by one position in the list
 
     # Update the x,y position of the snakes head and make a new coordinate
 
